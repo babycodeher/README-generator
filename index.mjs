8c251bc9651a53150e795d265390853c9f23a5f9
Wumi Afo <117525566+babycodeher@users.noreply.github.com>
2023-02-19 13:17:54 +0000

    set up index.mjs file and installed npm

---
+++ index.mjs
@@ -0,0 +1,31 @@
+import inquirer from 'inquirer';
+// import fs from "fs/promises";
+
+let { title, description, } = await inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: "What's the title of your project?",
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: "Write a short description of the project",
+            default() {
+                return 'Not applicable';
+            },
+        },
+    ])
+
+// `# Project Title
+// ${title}
+
+
+// ## Project Description
+// ${description}
+
+
+// ###### The smallest heading`
+
+console.log(title, description);
